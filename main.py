e04ce3051c99417c4189ffe74865e83792eddce5
Jett Thompson <jett.thompson.59@gmail.com>
2022-09-15 19:48:40 +1000

    add inputs and code to generate a network to work with them

--- main.py
+++ main.py
@@ -2,26 +2,64 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images,
-                               test_labels) = fashion_mnist.load_data()
+def createInput(inputString):
+    inputArray = []
+    for char in inputString:
+        inputArray.append(float(ord(char) / 255.0))
+    # Fill to 20 characters.
+    for i in range(20 - len(inputString)):
+        inputArray.append(0)
+    return inputArray
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+def createOutput(outputString):
+    # 'true' - correct or 'false' - not correct
+    return [1.0, 0.0] if outputString == 'true' else [0.0, 1.0]
 
-model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(
-    28, 28)),    tf.keras.layers.Dense(128, activation='relu'),    tf.keras.layers.Dense(10)])
 
-model.compile(optimizer='adam',              loss=tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True),              metrics=['accuracy'])
+def loadData():
+    inputFile = open('inputs.txt', 'r')
+    lines = inputFile.readlines()
+    inputs = []
+    outputs = []
+    for line in lines:
+        inputWords = line.split()
+        inputs.append(createInput(inputWords[0]))
+        outputs.append(createOutput(inputWords[1]))
+    trainingInputs = np.array(inputs[:int(len(inputs) * 0.8)])
+    trainingOutputs = np.array( outputs[:int(len(outputs) * 0.8)])
+    testingInputs = np.array(inputs[int(len(inputs) * 0.8):])
+    testingOutputs = np.array(outputs[int(len(outputs) * 0.8):])
+    return (trainingInputs, trainingOutputs), (testingInputs, testingOutputs)
 
-model.fit(train_images, train_labels, epochs=10)
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+(train_inputs, train_outputs), (test_inputs,
+                                test_outputs) = loadData()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+model.fit(train_inputs, train_outputs, epochs=20)
+print("Finished training.")
+
+test_loss, test_acc = model.evaluate(test_inputs,  test_outputs, verbose=2)
 
 print('\nTest accuracy:', test_acc)
+
+# Start an interractive session where the user types in words and the model figures out if they are correctly spelled.
+while True:
+    inputString = input("Enter a word: ")
+    if inputString == "exit":
+        break
+    inputArray = createInput(inputString)
+    inputArray = np.array([inputArray])
+    output = model.predict(inputArray)
+    print("Correctly spelled: " + str(output[0][0] > output[0][1]))
