1204d91f6e51860ce374fd9a8b068d640692f360
Jett Thompson <jett.thompson.59@gmail.com>
2023-05-21 13:28:07 +1000

    update the articles.py to use a proper recurrant system

--- articles.py
+++ articles.py
@@ -1,109 +1,126 @@
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import models
+from tensorflow.keras.layers import Dense, Embedding, GRU
+from tensorflow.keras.models import Sequential
 import numpy as np
+import os
 
-INPUT_INDICES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:?! \n"
+# Read the data
+text = open('articles.txt', 'rb').read().decode(encoding='utf-8')
 
-INPUT_CHARACTER_SIZE = len(INPUT_INDICES)
+# Create a unique sorted list of all characters in the text
+vocab = sorted(set(text))
 
-# Use a smaller data type for the outputArray array
-def generateInputsForCharacter(character, outputArray):
-    index = INPUT_INDICES.find(character)
-    if index == -1:
-        raise Exception("Invalid character: " + character + " (" + str(ord(character)) + ")")
-    outputArray[index] = np.int8(1)
-    return outputArray
+# Create a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
 
+# Convert the text to a sequence of character indices
+text_as_int = np.array([char2idx[c] for c in text])
 
-# Use a smaller data type for the inputs array
-def getCharacterForInput(inputs):
-    index = np.argmax(inputs)
-    return INPUT_INDICES[index]
+# The maximum length sentence we want for a single input in characters
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
 
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
 
-# Use a smaller data type for the outputArray array
-def generateInputsForString(str, outputArray):
-    index = 0
-    stringLength = len(str)
-    while index < stringLength:
-        character = str[index]
-        inputsForCharacter = generateInputsForCharacter(character, outputArray[index])
-        index += 1
-    return outputArray
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
 
+dataset = sequences.map(split_input_target)
 
-INPUT_SIZE = 200
-OUTPUT_SIZE = 1
+# Batch size
+BATCH_SIZE = 64
 
+# Buffer size to shuffle the dataset
+BUFFER_SIZE = 10000
 
-def loadData(path="articles.txt"):
-    with open(path) as f:
-        data = f.read()
-        # Use a smaller data type for the inputs array
-        inputs = np.zeros((len(data) // (INPUT_SIZE + OUTPUT_SIZE), INPUT_SIZE, INPUT_CHARACTER_SIZE), dtype=np.int8)
-        # Use a smaller data type for the outputs array
-        outputs = np.zeros((len(data) // (INPUT_SIZE + OUTPUT_SIZE), OUTPUT_SIZE, INPUT_CHARACTER_SIZE), dtype=np.int8)
-        i = 0
-        for offset in range(0, len(data) - INPUT_SIZE - OUTPUT_SIZE, INPUT_SIZE + OUTPUT_SIZE):
-            generateInputsForString(data[offset:offset+INPUT_SIZE], inputs[i])
-            generateInputsForString(
-                data[offset+INPUT_SIZE:offset+INPUT_SIZE+OUTPUT_SIZE], outputs[i])
-            i += 1
-        return inputs.reshape((len(inputs), INPUT_SIZE * INPUT_CHARACTER_SIZE)), outputs.reshape((len(outputs), OUTPUT_SIZE * INPUT_CHARACTER_SIZE))
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
 
-inputs, outputs = loadData()
-trainingInputs, trainingOutputs = inputs[:int(
-    len(inputs)*0.8)], outputs[:int(len(outputs)*0.8)]
-testingInputs, testingOutputs = inputs[int(
-    len(inputs)*0.8):], outputs[int(len(outputs)*0.8):]
+# The embedding dimension
+embedding_dim = 256
 
+# Number of RNN units
+rnn_units = 1024
 
+# Define the model
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = Sequential([
+    Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
+    GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+    Dense(vocab_size)
+  ])
+  return model
 
-def createModel():
-    global model
-    model = models.Sequential()
-    model.add(layers.Dense(INPUT_SIZE * INPUT_CHARACTER_SIZE, activation='relu'))
-    model.add(layers.Dense(OUTPUT_SIZE *
-              INPUT_CHARACTER_SIZE, activation='softmax'))
+# Define the loss function
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+# Function to build and train the model
+def train_model(model):
+    model.compile(optimizer='adam', loss=loss, metrics=['accuracy'])
+    EPOCHS=5
+    history = model.fit(dataset, epochs=EPOCHS)
+    return model
 
-def loadModel():
-    global model
-    model = models.load_model(input("Model path: ").strip())
+# Function to save the model
+def save_model(model):
+    model.save_weights('article_model.ckpt')
 
+# Function to load the model
+def load_model(batch_size):
+    model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=batch_size)
+    model.load_weights('article_model.ckpt')
+    return model
 
-requestedAction = input("1 to load or 2 to start afresh")
-if requestedAction == "1":
-    loadModel()
-else:
-    createModel()
+# Function to generate text
+def generate_text(model, start_string):
+    num_generate = 1000
+    input_eval = [char2idx[s] for s in "@START" + start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+    text_generated = ''
+    temperature = 1.0
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        predictions = tf.squeeze(predictions, 0)
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+        input_eval = tf.expand_dims([predicted_id], 0)
+        text_generated += idx2char[predicted_id]
+        if text_generated.endswith('@END'):
+            text_generated = text_generated[:-len('@END')]
+            break
+    return (start_string + ''.join(text_generated))
 
+trainable_model = build_model(vocab_size, embedding_dim, rnn_units, BATCH_SIZE)
 
+# Interactive session
 while True:
-    inputString = input("Type the beginning: ")
-    if inputString == "train":
-        model.fit(trainingInputs, trainingOutputs, epochs=1, batch_size=64)
-        print(model.evaluate(testingInputs, testingOutputs))
-        continue
-    if inputString == "save":
-        models.save_model(model, input("Model path: ").strip())
-        continue
-    modelInput = np.zeros((INPUT_SIZE, INPUT_CHARACTER_SIZE), dtype=np.int8)
-    generateInputsForString(inputString.rjust(INPUT_SIZE, " "), modelInput)
-    tempModelInput = modelInput
-    outputCharacterCount = 0
-    while outputCharacterCount < 200:
-        rawOutput = model(tempModelInput.reshape(
-            1, INPUT_SIZE * INPUT_CHARACTER_SIZE), training=False)
-        outputCharacter = getCharacterForInput(rawOutput)
-        print(outputCharacter, end="", flush=True)
-        characterOutputArray = np.zeros(INPUT_CHARACTER_SIZE)
-        tempModelInput = np.append(
-            tempModelInput[1:], generateInputsForCharacter(outputCharacter, characterOutputArray).reshape(1, INPUT_CHARACTER_SIZE), axis=0)
-        outputCharacterCount += 1
-    print("")
+    command = input("Enter a command (train/load/generate/quit): ")
+    if command == 'train':
+        trainable_model = train_model(trainable_model)
+        save_model(trainable_model)
+        generation_model = load_model(1)
+    elif command == 'load':
+        if os.path.exists('article_model.ckpt.index'):
+            trainable_model = load_model(BATCH_SIZE)
+            generation_model = load_model(1)
+        else:
+            print("No saved model found.")
+    elif command == 'generate':
+        if 'generation_model' in locals():
+            start_string = input("Enter a starting string: ")
+            print(generate_text(generation_model, start_string=start_string))
+        else:
+            print("No model available. Train or load a model first.")
+    elif command == 'quit':
+        break
+    else:
+        print("Invalid command.")
